ef60fb6dd41e3052a29548a4c2d64330c975ee3b
francesca-golzio <francescagolzio@gmail.com>
2026-07-03 16:14:04 +0200

    add form

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Password Generator</title>
-
+  
+  <!-- Custom CSS -->
   <style>
 
     body{
@@ -22,6 +23,19 @@
 
 </head>
 <body>
+
+  <h1>Password Generator</h1>
+  
+  <div>
+    <form action="" method="get">
+
+      <input type=text name="password_lenght" id="password_lenght" placeholder="Desired Password Lenght">
+      
+      <button type="submit">Generate Password</button>
+
+    </form>
+  </div>
+
   
 </body>
 </html>
