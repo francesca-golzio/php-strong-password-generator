4bed9f8690b08fb496ef00475c9b43d1c81cf599
francesca-golzio <francescagolzio@gmail.com>
2026-07-03 15:41:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Password Generator</title>
+
+  <style>
+
+    body{
+      background-color: #c0c0c0;
+    }
+
+
+  </style>
+
+</head>
+<body>
+  
+</body>
+</html>
