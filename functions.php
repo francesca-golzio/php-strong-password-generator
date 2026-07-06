4b2d193aa06df257fc7544af77807711c01cb657
francesca-golzio <francescagolzio@gmail.com>
2026-07-06 10:18:31 +0200

    refactor: decouple generatePassword() from request data
    the function now requires a length parameter

--- functions.php
+++ functions.php
@@ -2,11 +2,7 @@
 
 
   // Generate Password
-  function generatePassword(){
-    
-    /* Get password_length if properly setted */
-    (isset($_GET['password_length']) && (int)$_GET['password_length'] > 0) ? $password_length = (int)$_GET['password_length'] : $password_length = null;    
-    //var_dump($password_length);
+  function generatePassword($length){
 
     /* Define password possible characters */
     $chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '?', '_', '*']; 
@@ -16,7 +12,7 @@
     $password = '';
     
     /* Get password characters by characters */
-    while (strlen($password) < $password_length) {
+    while (strlen($password) < $length) {
 
       /* Get random index between 0 and 66 (chars[] length) */
       $random_index = rand(0, 65);
